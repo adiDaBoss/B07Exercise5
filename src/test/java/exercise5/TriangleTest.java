74a7bf068d8277f9eccd18bb5d7fd378b1f77b08
Adeola Oloruntobi <adeolaoloruntobi64@gmail.com>
2026-06-27 18:49:12 -0400

    test: implemented tests for Rectangle.Java

--- src/test/java/exercise5/TriangleTest.java
+++ src/test/java/exercise5/TriangleTest.java
@@ -3,7 +3,6 @@ package exercise5;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -48,7 +47,7 @@ public class TriangleTest {
     @Test
     void equals_Null_False() {
         Point p1 = new Point(2, 3);
-        assertNotNull(p1);
+        assertNotEquals(p1, null);
     }
 
     @Test
